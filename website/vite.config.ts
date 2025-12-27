b5ea205f8d5cb4752634a05999f2b91c1da9f074
Ahsan Mahmood (aoneahsan) <aoneahsan@gmail.com>
2025-12-27 15:02:52 +0500

    working on project

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -24,5 +25,5 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 2000,
   },
-  logLevel: 'error',
+  logLevel: 'info',
 });
